73119c8a3e046c8f0f262698cfcabb100bd58370
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-04-28 18:43:12 +0900

    chore: remove if

--- main.ts
+++ main.ts
@@ -30,25 +30,19 @@ export default class EasyUniqueNotePlugin extends Plugin {
 		const uniqueNotePath = `${baseDir}/${window.moment().format("YYYY-MM-DD-HH-mm-ss")}.md`;
 		const currentTime = window.moment().format("HH:mm");
 
-		if (todayFile) {
-			const uniqueFile = await this.app.vault.create(
-				`${uniqueNotePath}`,
-				`- [[${this.getCanonicalFileName(todayPath)}]] ${currentTime}`,
-			);
-			this.app.vault.append(
-				todayFile,
-				`- ${currentTime} [[${this.getCanonicalFileName(uniqueNotePath)}]] `,
-			);
+		const uniqueFile = await this.app.vault.create(
+			`${uniqueNotePath}`,
+			`- [[${this.getCanonicalFileName(todayPath)}]] ${currentTime}`,
+		);
+		this.app.vault.append(
+			todayFile,
+			`- ${currentTime} [[${this.getCanonicalFileName(uniqueNotePath)}]] `,
+		);
 
-			const leaf = this.app.workspace.getLeaf(false);
-			// https://docs.obsidian.md/Reference/TypeScript+API/WorkspaceLeaf/openFile
-			// https://liamca.in/Obsidian/API+FAQ/views/focus+the+note+title+with+the+cursor
-			await leaf.openFile(uniqueFile, { eState: { rename: "end" } });
-		} else {
-			new Notice(
-				"Please create a journal for today first. The journal file should be in the format `YYYY-MM-DD.md`.",
-			);
-		}
+		const leaf = this.app.workspace.getLeaf(false);
+		// https://docs.obsidian.md/Reference/TypeScript+API/WorkspaceLeaf/openFile
+		// https://liamca.in/Obsidian/API+FAQ/views/focus+the+note+title+with+the+cursor
+		await leaf.openFile(uniqueFile, { eState: { rename: "end" } });
 	}
 
 	private getTodayFileAndPath() {
